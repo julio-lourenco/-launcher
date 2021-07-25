dc6293d3cedc8f01ce2b6ae46a3f921febff4271
Guilherme Carvalho <guilhermescarvalho@protonmail.com>
2021-07-25 19:48:57 -0300

    1 - Implementação de codificação MD5

--- app/src/main/java/com/blueoceansolutions/launcher/ValidaTelaApps.java
+++ app/src/main/java/com/blueoceansolutions/launcher/ValidaTelaApps.java
@@ -9,9 +9,13 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ValidaTelaApps extends AppCompatActivity {
 
-    private static final String senha = "1234"; // senha de teste haha
+    private String senha = getHashMd5("1234"); // senha de teste haha
     private EditText editSenha;
     private Button btnEntrar;
 
@@ -25,16 +29,32 @@ public class ValidaTelaApps extends AppCompatActivity {
 
     public void abrirApps(View view) {
         editSenha = findViewById(R.id.editSenha);
+        String editSenhaMD5 = getHashMd5(editSenha.getText().toString());
 
         System.out.println("Senha do Edit: " + editSenha.getText().toString());
+        System.out.println("Senha MD5 do Edit: " +  getHashMd5(editSenha.getText().toString()));
+
         System.out.println("Senha do app: "  + senha);
 
-        if(editSenha.getText().toString().equals(senha)) {
+
+
+        if(editSenhaMD5.equals(senha)) {
             Intent intent = new Intent(this, ListaDeTodosOsApps.class);
             startActivity(intent);
         } else {
             Toast.makeText(getApplicationContext(), "Senha incorreta!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    public static String getHashMd5(String value) {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        BigInteger hash = new BigInteger(1, md.digest(value.getBytes()));
+        return hash.toString(16);
     }
 
 }
