0b067180d6bc2edbf18ee0ac9c3b4bf0b9c38e9a
juliocesar.alves <liliany10adm@gmail.com>
2021-07-27 00:14:23 -0300

    adicionando uma validacao para procurar o pacote caso nao ache pelo nome do pacote o app

--- app/src/main/java/com/blueoceansolutions/launcher/MainActivity.java
+++ app/src/main/java/com/blueoceansolutions/launcher/MainActivity.java
@@ -14,20 +14,26 @@ import android.view.View;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listar();
+        //listar();
     }
 
-    public void abrirApp(View view, String packageNameToOpen){
+    public void abrirApp(View view, String packageNameToOpen, String label){
         try{
             Intent intent;
             new Intent(Intent.ACTION_MAIN);
             PackageManager managerClock = getPackageManager();
             intent = managerClock.getLaunchIntentForPackage(packageNameToOpen);
+            if(intent == null){
+                String nomePacote = getPackageNameByLabel(label);
+                if(!nomePacote.equals(null)) {
+                    intent = managerClock.getLaunchIntentForPackage(nomePacote);
+                }
+            }
             intent.addCategory(Intent.CATEGORY_LAUNCHER);
 
             startActivity(intent);
@@ -35,19 +41,15 @@ public class MainActivity extends AppCompatActivity {
             new AlertDialog.Builder(view.getContext())
                     .setTitle("Erro")
                     .setMessage("Erro ao tentar abrir aplicativo, verifique se ele está instalado no POS!")
-                    .setPositiveButton(android.R.string.yes, null )
+                    .setPositiveButton(android.R.string.yes, null)
                     .show();
         }
     }
 
-    public void abrirEmprestimo(View view){ abrirApp(view,"com.blueoceansolutions.gerenciadordeemprestimo"); }
-    public void abrirGestao(View view){ abrirApp(view,"br.com.ellotech"); }
-    public void abrirRealCap(View view){
-        abrirApp(view,"br.net.realcap");
-    }
-    public void abrirPagamento(View view){
-        abrirApp(view,"br.com.celerpay.tkpppos");
-    }
+    public void abrirEmprestimo(View view){ abrirApp(view,"com.blueoceansolutions.gerenciadordeemprestimo", "solicitar empréstimos"); }
+    public void abrirGestao(View view){ abrirApp(view,"br.com.ellotech", "pdv pos pay"); }
+    public void abrirRealCap(View view){ abrirApp(view,"br.net.realcap","real cap"); }
+    public void abrirPagamento(View view){ abrirApp(view,"br.com.celerpay.tkpppos","pagamento"); }
 
 
     public void abrirValidaApps(View view) {
@@ -70,13 +72,27 @@ public class MainActivity extends AppCompatActivity {
         return icon;
     }*/
 
-    private void listar(){
+    private String getPackageNameByLabel(String label){
+        final PackageManager pm = getPackageManager();
+        String packageName = null;
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo packageInfo : packages) {
+            if(label.toString().toLowerCase().equals(packageInfo.loadLabel(pm).toString().toLowerCase())){
+                packageName = packageInfo.packageName;
+            }
+        }
+
+        return packageName;
+    }
+
+   /* private void listar(){
         final PackageManager pm = getPackageManager();
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
 
         for (ApplicationInfo packageInfo : packages) {
             Log.d(ContentValues.TAG, packageInfo.loadLabel(pm)+":" + packageInfo.packageName);
         }
-    }
+    }*/
 
 }
